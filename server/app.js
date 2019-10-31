ead57ecca7a7b864aab45b665c455568c09f3842
pputra <mynameisyoga@live.com>
2019-10-30 18:20:15 -0700

    first commit

---
+++ server/app.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+
+const root = require('./routes');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', root);
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
+
+module.exports = app;

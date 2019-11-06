46623876533d5877aeb36a60817b395df6449e3f
pputra <mynameisyoga@live.com>
2019-11-05 17:31:47 -0800

    feat: add state dropdown option

--- client/src/app/weather-search-form/weather-search-form.component.ts
+++ client/src/app/weather-search-form/weather-search-form.component.ts
@@ -11,6 +11,7 @@ import {Router} from '@angular/router';
 export class WeatherSearchFormComponent implements OnInit {
   addressForm;
   showErrorMessages;
+  statesOptions;
   constructor(
     private formBuilder: FormBuilder,
     private weatherService: WeatherService,
@@ -26,6 +27,61 @@ export class WeatherSearchFormComponent implements OnInit {
       street: false,
       city: false,
       state: false,
+    };
+
+    this.statesOptions = {
+      "": "Select State",
+      AL: 'Alabama',
+      AK: 'Alaska',
+      AZ: 'Arizona',
+      AR: 'Arkansas',
+      CA: 'California',
+      CO: 'Colorado',
+      CT: 'Connecticut',
+      DE: 'Delaware',
+      DC: 'District Of Columbia',
+      FL: 'Florida',
+      GA: 'Georgia',
+      HI: 'Hawaii',
+      ID: 'Idaho',
+      IL: 'Illinois',
+      IN: 'Indiana',
+      IA: 'Iowa',
+      KS: 'Kansas',
+      KY: 'Kentucky',
+      LA: 'Louisiana',
+      ME: 'Maine',
+      MD: 'Maryland',
+      MA: 'Massachusetts',
+      MI: 'Michigan',
+      MN: 'Minnesota',
+      MS: 'Mississippi',
+      MO: 'Missouri',
+      MT: 'Montana',
+      NE: 'Nebraska',
+      NV: 'Nevada',
+      NH: 'New Hampshire',
+      NJ: 'New Jersey',
+      NM: 'New Mexico',
+      NY: 'New York',
+      NC: 'North Carolina',
+      ND: 'North Dakota',
+      OH: 'Ohio',
+      OK: 'Oklahoma',
+      OR: 'Oregon',
+      PA: 'Pennsylvania',
+      RI: 'Rhode Island',
+      SC: 'South Carolina',
+      SD: 'South Dakota',
+      TN: 'Tennessee',
+      TX: 'Texas',
+      UT: 'Utah',
+      VT: 'Vermont',
+      VA: 'Virginia',
+      WA: 'Washington',
+      WV: 'West Virginia',
+      WI: 'Wisconsin',
+      WY: 'Wyoming',
     };
   }
 
@@ -48,7 +104,12 @@ export class WeatherSearchFormComponent implements OnInit {
   }
 
   onResetForm() {
-    this.addressForm.reset();
+    this.addressForm = this.formBuilder.group({
+      street: '',
+      city: '',
+      state: '',
+    });
+  
     this.showErrorMessages = {
       street: false,
       city: false,

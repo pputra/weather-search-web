f85114c5be0524b86df6aa3ad4879f6ee409d2b7
pputra <mynameisyoga@live.com>
2019-11-06 23:10:48 -0800

    feat: add hourly chart

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { ChartsModule } from 'ng2-charts'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -25,6 +26,7 @@ import { HourlyWeatherComponent } from './hourly-weather/hourly-weather.componen
     AppRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
+    ChartsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- client/src/app/hourly-weather/hourly-weather.component.ts
+++ client/src/app/hourly-weather/hourly-weather.component.ts
@@ -7,9 +7,50 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class HourlyWeatherComponent implements OnInit {
   @Input() hourlyData;
+  activeChart;
+  chartOptions;
+  chartConfigs;
+  barChartOptions = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  };
+  barChartLabels = [ ...Array(24).keys() ];
+  barChartType = 'bar';
+  barChartLegend = true;
 
-  constructor() { }
+  constructor() { 
+    this.activeChart = 'temperature'
+    this.chartOptions = {
+      Temperature: 'temperature',
+      Pressure: 'pressure',
+      Humidity: 'humidity',
+      Ozone: 'ozone',
+      Visibility: 'visibility',
+      "Wind Speed": 'windSpeed',
+    };
 
+    this.chartConfigs = {
+      temperature: this.createChartConfig('temperature', [], 'Fahrenheit'),
+      pressure: this.createChartConfig('pressure', [], 'Millibars'),
+      humidity: this.createChartConfig('humidity', [], '% Humidity'),
+      ozone: this.createChartConfig('ozone', [], 'Dobson Units'),
+      visibility: this.createChartConfig('visibility', [], 'Miles (Maximum 10)'),
+      windSpeed: this.createChartConfig('windSpeed', [], 'Miles per Hour'),
+    };
+  }
+
+  createChartConfig(label, data, yAxisLabel) {
+    return { data: [ { data: [28, 48, 40, 19, 86, 27, 90], label: label } ], yAxisLabel: yAxisLabel };
+  }
+
+  setActiveChart(dataType) {
+    this.activeChart = dataType;
+  }
+
+  sortObjectByIndex() {
+    return 0;
+  }
+  
   ngOnInit() {
     
   }

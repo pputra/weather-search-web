077220b87c08e3da143e8705cf237fb751ec8fe6
pputra <mynameisyoga@live.com>
2019-11-07 12:04:10 -0800

    fix: search engine url issue

--- server/actions/searchEngine.action.js
+++ server/actions/searchEngine.action.js
@@ -3,10 +3,8 @@ const request = require('request-promise');
 module.exports = {
   getStateSeal: async (state) => {
       try {
-        const uri = `https://www.googleapis.com/customsearch/v1?q=${state}%20State%20Seal
-          &cx=${process.env.SEARCH_ENGINE_ID}&imgSize=huge&imgType=news&num=1
-          &searchType=image&key=${process.env.GOOGLE_API_KEY}`;
-  
+        const uri = `https://www.googleapis.com/customsearch/v1?q=${state}%20State%20Seal&cx=${process.env.SEARCH_ENGINE_ID}&imgSize=huge&imgType=news&num=1&searchType=image&key=${process.env.GOOGLE_API_KEY}`;
+
         const options = {
           uri,
           method: 'GET',

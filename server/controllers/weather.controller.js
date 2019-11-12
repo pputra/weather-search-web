f277e1e17db7b998d1d97a94ccd0b2c33a6c23f6
pputra <mynameisyoga@live.com>
2019-11-11 21:21:14 -0800

    feat: add req query for state steal

--- server/controllers/weather.controller.js
+++ server/controllers/weather.controller.js
@@ -12,6 +12,7 @@ module.exports = {
         lat,
         lon,
         time,
+        state_seal,
       } = req.query;
 
       let weatherData;
@@ -25,17 +26,24 @@ module.exports = {
         weatherData = await getWeatherByCoordinate(latitude, longitude);
       }
 
-      //const stateSeal = await getStateSeal(state);
+      const fetchStateSeal = state_seal === 'true';
+      if (fetchStateSeal) {
+        const stateSeal = await getStateSeal(state);
+        //const stateSeal = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Seal_of_New_York.svg/2000px-Seal_of_New_York.svg.png";
 
-      // TESTING PURPOSE
-      const stateSeal = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Seal_of_New_York.svg/2000px-Seal_of_New_York.svg.png";
+        return res.status(200).json({
+          message: "weather data has been fetched",
+          stateSeal,
+          weatherData,
+        });
+      }
 
       return res.status(200).json({
         message: "weather data has been fetched",
-        stateSeal,
         weatherData,
       });
     } catch (err) {
+
       return res.status(401).json({ message: err.message });
     }
   },
